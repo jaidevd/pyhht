053d2b44f4b2333a8c6364cf93b5f9cfd4927098
Jaidev Deshpande <deshpande.jaidev@gmail.com>
2015-01-10 20:04:57 +0530

    Begin reorganization

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+NAME = "pyhht"
+
+setup(
+    name=NAME,
+    version='0.0.1',
+    author='Jaidev Deshpande',
+    author_email='deshpande.jaidev@gmail.com',
+    packages=['pyhht']
+)
