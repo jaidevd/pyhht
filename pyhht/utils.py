90a04236a6da6e9364a518c37f0d20f96ec88cad
Jaidev Deshpande <deshpande.jaidev@gmail.com>
2015-02-08 07:19:28 +0530

    Move the EMD.extr function to utils.py

--- pyhht/utils.py
+++ pyhht/utils.py
@@ -13,6 +13,7 @@ Utility functions used to inspect EMD functionality.
 import matplotlib.pyplot as plt
 from matplotlib.mlab import find
 import numpy as np
+from scipy.signal import argrelmax, argrelmin
 
 def get_envelops(x, t=None):
     """ Find the upper and lower envelopes of the array `x`.
@@ -50,70 +51,28 @@ def plot_imfs(imfs, shape=None):
     plt.show()
 
 
-def extr(x):
-    """ Extracts the indices of the extrema and zero crossings. """
-    m = len(x)
+def extr(self, x):
+        """ Extracts the indices of the extrema and zero crossings. """
+        # FIXME: This doesn't have to be a method here.
+        m = x.shape[0]
 
-    x1 = x[:m-1]
-    x2 = x[1:m]
-    indzer = find(x1*x2<0)
-    if np.any(x==0):
-        iz = find(x==0)
-        indz = [];
-        if np.any(np.diff(iz)==1):
-            zer = x == 0
-            dz = np.diff([0,zer,0])
-            debz = find(dz == 1)
-            finz = find(dz == -1)-1
-            indz = np.round((debz+finz)/2)
-        else:
-            indz = iz
-        indzer = np.sort(np.hstack([indzer,indz]))
-
-    d = np.diff(x)
-
-    n = len(d)
-    d1 = d[:n-1]
-    d2 = d[1:n]
-    indmin = set(find(d1*d2<0)).intersection(find(d1<0))
-    indmin = np.array(list(indmin)) + 1
-    indmax = set(find(d1*d2<0)).intersection(find(d1>0))
-    indmax = np.array(list(indmax)) + 1
-
-    if np.any(d==0):
-        imax = []
-        imin = []
-        bad = (d==0)
-        dd = np.diff([0,bad,0])
-        debs = find(dd == 1)
-        fins = find(dd == -1)
-        if debs[0] == 1:
-            if len(debs) > 1:
-                debs = debs[2:]
-                fins = fins[2:]
+        x1 = x[:m-1]
+        x2 = x[1:m]
+        indzer = find(x1 * x2 < 0)
+        if np.any(x == 0):
+            iz = find(x == 0)
+            indz = []
+            if np.any(np.diff(iz) == 1):
+                zer = x == 0
+                dz = np.diff([0, zer, 0])
+                debz = find(dz == 1)
+                finz = find(dz == -1) - 1
+                indz = np.round((debz + finz) / 2)
             else:
-                debs = []
-                fins = []
-        if len(debs) > 0:
-            if fins(len(fins)-1) == m:
-                if len(debs)>1:
-                    debs = debs[:len(debs)-1]
-                    fins = fins[:len(fins)-1]
-                else:
-                    debs = []
-                    fins = []
-        lc = len(debs)
-        if lc > 0:
-            for k in range(lc):
-                if d[debs[k]-1]>0:
-                    if d[fins[k]] < 0:
-                        imax = [imax,np.round((fins[k]+debs[k])/2)]
-                else:
-                    if d[fins[k]]>- 0 :
-                        imin = [imin, np.round((fins[k]+debs[k])/2)]
-        
-        if len(imax)>0:
-            indmax = np.sort([indmax,imax])
-        if len(imin)>0:
-            indmin = np.sort([indmin, imin])
-    return indmin, indmax, indzer
+                indz = iz
+            indzer = np.sort(np.hstack([indzer, indz]))
+
+        indmax = argrelmax(x)[0]
+        indmin = argrelmin(x)[0]
+
+        return indmin, indmax, indzer
