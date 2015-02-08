65e7ea5dc94d1e5e598d20112d573dfcdce5f927
Jaidev Deshpande <deshpande.jaidev@gmail.com>
2015-02-09 04:39:50 +0530

    Fix tests for extrema and zero crossing detection

--- pyhht/tests/test_utils.py
+++ pyhht/tests/test_utils.py
@@ -13,7 +13,6 @@ Tests for the basic utility functions in `pyhht.utils`
 import unittest
 from pyhht import utils
 import numpy as np
-from numpy.testing import assert_allclose
 
 
 class TestUtils(unittest.TestCase):
@@ -21,18 +20,31 @@ class TestUtils(unittest.TestCase):
         t = np.linspace(0, 1, 1000)
         self.data = np.sin(2 * np.pi * 5 * t)
 
-    def test_extr_zerocross(self):
+    def test_extrema(self):
         """
-        Test if local extrema and zero crossings are detected properly.
+        Test if local extrema are detected properly.
         """
         # FIXME: Don't use the fact that the data is a sine wave.
         # Eg: Use the properties of immediate neighbours
-        # FIXME: Test zero crossings more carefully, the rest is just
-        # `scipy.argrelextrema`
-        indmin, indmax, indzer = utils.extr(self.data)
-        desired = np.ones((5,))
-        actual = self.data[indmax]
-        assert_allclose(actual, desired)
-        desired *= -1
-        actual = self.data[indmin]
-        assert_allclose(actual, desired)
+        indmin, indmax, _ = utils.extr(self.data)
+        min_neighbours = np.zeros((indmin.shape[0], 2))
+        max_neighbours = np.zeros((indmax.shape[0], 2))
+        min_neighbours[:, 0] = self.data[indmin - 1]
+        min_neighbours[:, 1] = self.data[indmin + 1]
+        max_neighbours[:, 0] = self.data[indmax - 1]
+        max_neighbours[:, 1] = self.data[indmax + 1]
+        minima = self.data[indmin].reshape(indmin.shape[0], 1)
+        self.assertTrue(np.all(min_neighbours >= minima))
+        maxima = self.data[indmax].reshape(indmax.shape[0], 1)
+        self.assertTrue(np.all(max_neighbours <= maxima))
+
+    def test_zerocrossings(self):
+        """
+        Test if the zero crossings are accurate.
+        """
+        _, _, indzer = utils.extr(self.data)
+        neighbours = np.zeros((indzer.shape[0], 2))
+        neighbours[:, 0] = self.data[indzer - 1]
+        neighbours[:, 1] = self.data[indzer + 1]
+        p = np.prod(neighbours, axis=1)
+        self.assertTrue(np.all(p < 0))
