3343435c87482eafc94dac6cca48440e174180a4
Jaidev Deshpande <deshpande.jaidev@gmail.com>
2015-06-21 06:06:22 +0530

    Use inline code for plotting gaussian signal example

--- docs/source/tutorials/plots/gaussian_signal.py
+++
@@ -1,9 +0,0 @@
-from tftb.generators import fmconst, amgauss
-import matplotlib.pyplot as plt
-from numpy import real
-x = amgauss(128) * fmconst(128)[0]
-plt.plot(real(x))
-plt.grid()
-plt.xlim(0, 128)
-plt.title("Gaussian amplitude modulation")
-plt.show()
