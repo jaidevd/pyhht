6834d6be599d6fd997f642e72a09f2bbb547ae0a
Jaidev Deshpande <deshpande.jaidev@gmail.com>
2016-01-14 15:25:39 +0530

    Fix CI script
    Conda command wasn't being found.

--- ci/install.sh
+++ ci/install.sh
@@ -19,8 +19,9 @@ if [[ "$DISTRIB" == "conda" ]]; then
 	# itself
 	wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh \
 		-O miniconda.sh
-    chmod +x miniconda.sh && ./miniconda.sh -b
+    bash miniconda.sh -b -p $HOME/miniconda
     export PATH=$HOME/miniconda/bin:$PATH
+    hash -r
 	conda config --set always_yes yes --set changeps1 no
 	conda update conda
 	conda info -a
